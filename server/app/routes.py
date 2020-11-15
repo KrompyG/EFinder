29bb0adfa05d59e0ce4459f95b05cd2b6b7bbd33
Platonov Max <xamtalp@gmail.com>
2020-11-15 16:18:38 +0300

    [add] text recognition

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,10 +1,12 @@
 from app import app
 from flask import request
 from werkzeug.utils import secure_filename
-import os.path
+import io
+import os
+# Imports the Google Cloud client library
+from google.cloud import vision
 
-
-# checking file's extension
+# check file's extension
 def allowed_file(filename):
     return ('.' in filename and
             filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS'])
@@ -13,18 +15,34 @@ def allowed_file(filename):
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return "Hello, this is Efinder!"
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    imageFile = request.files.get('imagefile', '') 
+    # getting image file from request
+    imageFile = request.files['imagefile']
+
+    # checking file existance and its extension
     if imageFile and allowed_file(imageFile.filename):
-        filename = secure_filename(imageFile.filename)
+        # Instantiates a client
+        client = vision.ImageAnnotatorClient()
+        # creating Image for sending it to Google
+        image = vision.Image(content=imageFile.read())
+        # Performs label detection on the image file
+        # pylint: disable=no-member
+        response = client.text_detection(image=image,
+            image_context={"language_hints": ["ru"]})
+        # getting recognised text from response
+        texts  = response.text_annotations
 
-        # place here code for sending photo to Google API
+        # texts[0] - весь распознанный текст целиком
+        # начиная с texts[1] - отдельные слова
+        for text in texts:
+            print('\n"{}"'.format(text.description))
 
-        imageFile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        return "image received successfully"
+        # TODO: write here code that finds E additives
+        # and send them back to client
+        return "image received successfully\n"
     
     return "failed to get an image"
--- server/config.py
+++ server/config.py
@@ -1,5 +1,8 @@
 import os
 
+# paste here path to your key.json
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/maxim/Documents/efinder/server/key.json"
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'strong-secret-key-here'
     ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
