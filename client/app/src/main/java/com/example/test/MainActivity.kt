1c74b17f8f4c60f57813483da86d1de2c1c99d43
Platonov Max <xamtalp@gmail.com>
2020-11-21 02:35:49 +0300

    [add] client photo sending

--- client/app/src/main/java/com/example/test/MainActivity.kt
+++ client/app/src/main/java/com/example/test/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.test
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import kotlinx.coroutines.*
-import khttp.get
 import android.content.Intent
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import khttp.get
+import khttp.post
+import khttp.structures.files.FileLike
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
     private val CAMERA_REQUEST_CODE = 0
-    private val GALLERY_REQUEST = 1;
+    private val GALLERY_REQUEST = 1
+    var BYTE_ARRAY_PHOTO = ByteArrayOutputStream()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +30,26 @@ class MainActivity : AppCompatActivity() {
 
     // here will be code for sending photo to the server
     fun sendPhoto(view: View){
-        val myToast = Toast.makeText(this, "halo", Toast.LENGTH_SHORT)
-        GlobalScope.launch{
-            get("https://www.google.com/")
+        val myToast = Toast.makeText(this, "no content", Toast.LENGTH_SHORT)
+        if(BYTE_ARRAY_PHOTO.size() == 0){
+            myToast.show()
+            return
+        }
+
+        GlobalScope.launch(Dispatchers.Main){
+            val postOperation = async(Dispatchers.IO){
+                //get("http://192.168.43.244:5000/")
+                post(
+                    url="http://192.168.43.244:5000/upload",
+                    files= listOf(
+                        FileLike("imagefile","photo.jpeg", BYTE_ARRAY_PHOTO.toByteArray())
+                    )
+                )
+            }
+
+            val response = postOperation.await()
+
+            myToast.setText(response.text)
             myToast.show()
         }
 
@@ -41,17 +65,23 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
         when (requestCode) {
             CAMERA_REQUEST_CODE -> {
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     // showing taken photo
-                    imageView.setImageBitmap(data.extras?.get("data") as Bitmap)
+                    val bitmap_photo = data.extras?.get("data") as Bitmap
+                    bitmap_photo.compress(Bitmap.CompressFormat.JPEG, 100, BYTE_ARRAY_PHOTO)
 
+                    imageView.setImageBitmap(bitmap_photo)
                 }
             }
             GALLERY_REQUEST -> {
                 if (resultCode == Activity.RESULT_OK && data != null) {
                     // showing uploaded photo
+                    val bitmap_photo = data.extras?.get("data") as Bitmap
+                    bitmap_photo.compress(Bitmap.CompressFormat.JPEG, 100, BYTE_ARRAY_PHOTO)
+
                     imageView.setImageURI(data.data)
                 }
             }
